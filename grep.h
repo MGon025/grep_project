3cb27a71d50aa2e8f4a6c78dcf8902e380c812bf
MGon025 <43521752+MGon025@users.noreply.github.com>
2019-05-16 18:05:58 -0700

    Update grep.h

--- grep.h
+++ grep.h
@@ -1,7 +1,7 @@
 #ifndef GREP_H
 #define GREP_H
 char *getblock(unsigned int atl);
-char *getline(unsigned int tl);
+char *getline_(unsigned int tl);
 int advance(char *lp, char *ep);
 int append(int (*f)(void), unsigned int *a);
 int cclass(char *set, int c, int af);
@@ -16,29 +16,19 @@ void init(void);
 void print(void);
 void putchr(int ac);
 int putline(void);
-int puts(char *sp);
 void setwide(void);
-void gvars(void);
 void pattern_init(char* p);
 int file_init(int arc, char* arv[]);
 int dir_list(char* dirv[]);
 void reset(int uf);
-int open(char *, int);
-int creat(char *, int);
-int read(int, char*, int);
-int write(int, char*, int);
-int close(int);
-int unlink(char *);
-char *mkdtemp(char *);
 
-#define	BLKSIZE	16384
+#define	BLKSIZE 16384
 #define	NBLK	2047
 #define	FNSIZE	128
 #define	LBSIZE	16384
 #define	ESIZE	256
 #define	GBSIZE	256
 #define	NBRA	5
-#define	EOF	-1
 #define	CBRA	1
 #define	CCHR	2
 #define	CDOT	4
@@ -50,8 +40,6 @@ char *mkdtemp(char *);
 #define	CBACK	14
 #define	CCIRC	15
 #define	STAR	01
-#define	READ	0
-#define	WRITE	1
 #define MAXFILES 10
 
 extern int	tfile, nbra, nleft, peekc, lastc, given, io, ninbuf, tline, ufile, gt1;
